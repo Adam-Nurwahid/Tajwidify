2e3933b0077aa15316f9bd4880397c0e12d6039c
Adam Toyib Nurwahid <138128937+Adam-Nurwahid@users.noreply.github.com>
2023-11-06 21:13:43 +0700

    vesi di gradle yg sebelumnya 33 jadi 34 biar jalan

---
+++ app/src/main/java/com/kuliah/pkm/tajwidify/MateriActivity.kt
@@ -0,0 +1,11 @@
+package com.kuliah.pkm.tajwidify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MateriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materi)
+    }
+}
---
+++ app/src/main/java/com/kuliah/pkm/tajwidify/MateriDataClass.kt
@@ -0,0 +1,37 @@
+package com.kuliah.pkm.tajwidify
+
+import android.os.Parcel
+import android.os.Parcelable
+import org.intellij.lang.annotations.Language
+
+data class MateriDataClass(
+    val name: String?,
+    val description: String?,
+    val photo: Int
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeInt(photo)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MateriDataClass> {
+        override fun createFromParcel(parcel: Parcel): MateriDataClass {
+            return MateriDataClass(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MateriDataClass?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
